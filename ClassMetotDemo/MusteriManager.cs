6d632ee8171f580f68e6e558ddc9e21c4292fb7d
Efecan <efecanbayat@gmail.com>
2021-03-02 18:09:58 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriler = new List<Musteri>();
+
+        public void Ekle(Musteri musteri)
+        {                      
+            musteriler.Add(musteri);
+            Console.WriteLine("Müşteri Eklendi : " + " " + musteri.Ad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine("Müşteri Silindi : " + " " + musteri.Ad);
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine("----------------Müşteri Listesi----------------");
+            foreach (Musteri musteri in musteriler)
+            {
+                
+                Console.WriteLine("Id :" + " " + musteri.Id);
+                Console.WriteLine("Ad :" + " " + musteri.Ad);
+                Console.WriteLine("Soyad :" + " " + musteri.Soyad);
+                Console.WriteLine("Yas :" + " " + musteri.Yas);
+                Console.WriteLine("--------------");
+            }
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Efecan";
+            musteri1.Soyad = "Bayat";
+            musteri1.Yas = 24;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Mücahit";
+            musteri2.Soyad = "Düz";
+            musteri2.Yas = 24;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Onur";
+            musteri3.Soyad = "Temel";
+            musteri3.Yas = 24;
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            musteriManager.Sil(musteri2);
+
+            musteriManager.Listele();
+
+            
+        }
+    }
+}
